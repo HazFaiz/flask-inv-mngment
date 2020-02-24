d22e5ae7e65c85486e200c244c2b94f412aa5666
HFLaptop <haz.faizul@gmail.com>
2020-02-24 18:18:09 +0800

    working form to database

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 import peeweedbevolve
-from flask import Flask, render_template, request
-from models import db  # new line
+from flask import Flask, render_template, request, flash, redirect, url_for
+from models import db, Store, Warehouse, Product  # new line
 app = Flask(__name__)
 
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 @app.before_request  # new line
 def before_request():
@@ -24,6 +25,26 @@ def migrate():  # new
 def index():
     return render_template('index.html')
 
+# Forms
+
+
+@app.route("/store", methods=["GET"])
+def show_store():
+    return render_template('store.html')
+
+
+@app.route("/store/new", methods=["POST"])
+def create():
+    store_name = request.form.get("store_name")
+    store = Store(name=store_name)
+
+    if store.save():
+        flash(f"Saved store: {store_name}")
+        return redirect(url_for("show_store"))
+    else:
+        flash("That name is already taken")
+        return render_template("store.html")
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
